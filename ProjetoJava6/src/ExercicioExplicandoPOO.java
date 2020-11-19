3a8d20db35c8b397c8922e94dcc658e79c1d0bdd
unknown <fsergioruiz@hotmail.com>
2020-11-19 19:25:19 -0300

    Exercicios 19/11

---
+++ ProjetoJava6/src/ExercicioExplicandoPOO.java
@@ -0,0 +1,58 @@
+import java.util.*;
+public class ExercicioExplicandoPOO {
+	public static void main(String args[]) {
+		Scanner ler = new Scanner(System.in);
+		String descricao;
+		System.out.println("Descreva em no máximo 200 palavras o que é um automóvel, uma pessoa e uma casa e o que eles fazem:\n" + "\nUm automovel é um meio de transporte composto de peças mecênicas, \r\n"
+				+ "que cada uma desenvolve uma fução, que juntas tem o objetivo de se deslocar \r\n"
+				+ "levando pessoas de um lugar a outro.\r\n"
+				+ "\r\n"
+				+ "Substantivos: Automovel,peças,pessoas, lugar. \r\n"
+				+ "Verbos:desenvolve, deslocar, levando.\r\n"
+				+ "\r\n"
+				+ "\r\n"
+				+ "Uma pessoa é um ser vivo que habita a Terra em sociedade com outras pessoas \r\n"
+				+ "visando um bem estar comum, e fazendo bem feitorias no planeta Terra.\r\n"
+				+ "\r\n"
+				+ "Substantivos: Pessoa, ser vivo, Terra, planeta Terra.    \r\n"
+				+ "Verbos: Habita, estar, fazendo, visando.\r\n"
+				+ "\r\n"
+				+ "Uma casa é o onde mora uma pessoa ou pelo menos deveria ser, \r\n"
+				+ "para todos aqueles que queiram, é o refúgio onde uma pessoa se abriga, \r\n"
+				+ "onde ela vive grande parte da vida, onde reunimos a familia.\r\n"
+				+ "\r\n"
+				+ "Substantivos: Casa, pessoa, familia  \r\n"
+				+ "Verbos: Mora, ser, queiram, vive, reunimos.\r\n"
+				+ "\r\n"
+				+ "Objetos: \r\n"
+				+ "-Automovel: \r\n"
+				+ "Atributos: Cor, Qtas portas,modelo.\r\n"
+				+ "Comportamento: Andando, parado, quebrado \r\n"
+				+ "-peças\r\n"
+				+ "Atributos: Peças do motor, do cambio, da carroceria.\r\n"
+				+ "Comportamentos: Quebradas, funcionando, novas, velhas\r\n"
+				+ "-pessoas\r\n"
+				+ "Atributos:baixa, alta, gorda, magra.\r\n"
+				+ "Comportamento: Respeitosa, sem educação, agradavel, desagradavel\r\n"
+				+ "-lugar\r\n"
+				+ "Atributos: Casa, parque, escola\r\n"
+				+ "Comportamento: Frio ou calor\r\n"
+				+ "-planeta Terra.\r\n"
+				+ "Atributos: Tem continentes, tem paízes, tem oceanos \r\n"
+				+ "Comportamento: Pode estar sendro preservado ou não por sua população.\r\n"
+				+ "\r\n"
+				+ "\r\n"
+				+ "Os objetos se entegram entre si, em uma vivencia cotidiana.\r\n"
+				+ "Pessoas vivem em um Planeta, moram em casas, que fica em algum lugar,\r\n"
+				+ "que possuim automóveis e que esses automóveis possuem peças.\r\n"
+				+ "\r\n"
+				+ "\r\n"
+				+ "Desculpem o português rsrsrs.");
+		descricao = ler.next();
+		
+		
+		
+		
+		
+	}
+}
